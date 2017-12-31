0cd8ff176bf8d9b770f7e93e8f3084e7a4a4e2aa
Michael Sleevi <michael.sleevi@gmail.com>
2017-12-31 08:42:25 -0700

    Conversion to React from Angular

---
+++ src/shared/component/message.jsx
@@ -0,0 +1,14 @@
+// @ flow
+
+import React from 'react'
+
+// eslint-disable-next-line
+type Props = {
+  message: string,
+}
+
+// eslint-disable-next-line
+const Message = ({ message }: Props) =>
+  <p>{message}</p>
+
+export default Message
