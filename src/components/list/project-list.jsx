4b99e743184ea123af220ba1f5f4b0943dfc00be
Michael Sleevi <michael.sleevi@gmail.com>
2018-01-20 20:46:04 -0700

    Refactoring controllers to be contained within components, blank page structures for the rest of the pages

---
+++ src/components/list/project-list.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class ProjectList extends React.Component {
+	render() {
+		return (
+			<div>
+				<h3>"Foo Bar"</h3>
+			</div>
+		)
+	}
+}
---
+++ src/components/page/cv/CV.jsx
@@ -0,0 +1,10 @@
+
+class CVPage extends React.Component({
+	render() {
+		return (
+			<div>
+				<h3>"Put Resume here"</h3>
+			</div>
+		)
+	}
+})
