557e1f8410a4e0f6d6dab77623ceb2787b586940
Michael Sleevi <michael.sleevi@gmail.com>
2018-07-02 16:19:31 -0600

    Refactor to get closer to what go projects should look like

---
+++ api/src/api/routes/error.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func HandleError(message string, code int, w http.ResponseWriter) {
+	w.WriteHeader(code)
+	w.Write([]byte(message))
+	log.Error(message)
+	return
+}
