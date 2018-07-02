73c334e0ab887030d50782edfc7a94973f174499
Michael Sleevi <michael.sleevi@gmail.com>
2018-07-02 16:37:46 -0600

    Fixing the config file

--- ui/src/constants/config.js
+++ ui/src/constants/config.js
@@ -2,7 +2,7 @@
 
 export const WEB_PORT = process.env.PORT || 8000
 export const STATIC_PATH = '/static'
-export const APP_NAME = 'Hello Application'
+export const APP_NAME = 'msleevi'
 export const WDS_PORT = 7000
 export const APP_CONTAINER_CLASS = 'js-app'
 export const APP_CONTAINER_SELECTOR = `.${APP_CONTAINER_CLASS}`
