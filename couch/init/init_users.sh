bbb83484b0e46c50b69137233d633f2a45759ad8
Michael Sleevi <michael.sleevi@gmail.com>
2018-07-05 18:44:31 -0600

    Attempting to create an init style

--- couch/init/init_users.sh
+++ couch/init/init_users.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-set -x
+set -ex
 
 function init_users() {
-    res=$(cat $1 | jq '.')
-    echo $res
-    for row in $(jq '.[]' $1); do
-        echo ${row}
-        echo ${row} | base64 --decode | jq -r ${1}
-        # curl -H 'Content-Type: application/json' \
-        #     -X PUT http://${COUCH_URL}:${COUCH_PORT}/${DATABASE} \
-        #     -D ${row}
-    done
+    while IFS="=" read -r key value; do
+        echo ${key}
+        echo ${value}
+        curl -H 'Content-Type: application/json' \
+            -X POST http://${COUCH_URL}:${COUCH_PORT}/${DATABASE}/ \
+            -D $value
+    done < <(jq -r ".users | to_entries | map(\"\(.value.id)=\(.value)\")|.[]" $1)
 }
 
 source ./environment.env
