b060f8854689d32d96e0c9fe5a60fbd84e52afa3
Michael Sleevi <michael.sleevi@gmail.com>
2018-07-09 11:28:25 -0600

    Commenting out the logger

--- ui/src/components/page/projects/projectsPage.controller.js
+++ ui/src/components/page/projects/projectsPage.controller.js
@@ -1,28 +1,28 @@
 // @flow
 
 import axios from 'axios'
-import logger from '../../../logging/logger'
+// import logger from '../../../logging/logger'
 
 
 function getProjects(endpoint: string) {
   return axios.get(endpoint)
     .then((response) => {
-      logger.info(response)
+      // logger.info(response)
     })
     .catch((error) => {
-      logger.error('Error')
-      logger.error(error)
+      // logger.error('Error')
+      // logger.error(error)
     })
 }
 
 function postProject(endpoint: string, project: Object) {
   return axios.post(endpoint, project)
     .then((response) => {
-      logger.info(response)
+      // logger.info(response)
     })
     .catch((error) => {
-      logger.error('Error')
-      logger.error(error)
+      // logger.error('Error')
+      // logger.error(error)
     })
 }
 
